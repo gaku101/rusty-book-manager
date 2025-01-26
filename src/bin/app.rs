370a1d311c1b6bcfc28bcb496a0002da9dceb624
gaku101 <takehiro-r133@hotmail.co.jp>
2025-01-26 11:04:50 +0900

    add log using opentelemetry

--- src/bin/app.rs
+++ src/bin/app.rs
@@ -17,6 +17,7 @@ use tracing_subscriber::util::SubscriberInitExt;
 use tracing_subscriber::EnvFilter;
 
 use anyhow::Context;
+use opentelemetry::global;
 use tower_http::trace::{DefaultMakeSpan, DefaultOnRequest, DefaultOnResponse, TraceLayer};
 use tower_http::LatencyUnit;
 use tracing::Level;
@@ -43,16 +44,37 @@ fn init_logger() -> Result<()> {
         Environment::Production => "info",
     };
 
-    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
+    // 環境変数の読み込み
+    let host = std::env::var("JAEGER_HOST")?;
+    let port = std::env::var("JAEGER_PORT")?;
+    let endpoint = format!("{host}:{port}");
 
+    global::set_text_map_propagator(opentelemetry_jaeger::Propagator::new());
+    // 1
+    let tracer = opentelemetry_jaeger::new_agent_pipeline()
+        .with_endpoint(endpoint)
+        .with_service_name("book-manager")
+        .with_auto_split_batch(true)
+        // 概ねこの程度のbytesを送れれば良いという値。アプリケーションのメッセージごとに変える。
+        // 足りないと、「Exporter jaeger encountered the following error(s): thrift agent failed with message too long」のようなメッセージが出る。
+        // Issueを参考に修正した: https://github.com/open-telemetry/opentelemetry-rust/issues/851
+        .with_max_packet_size(8192)
+        .install_simple()?;
+    // 2
+    let opentelemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    let env_filter = EnvFilter::try_from_default_env().unwrap_or_else(|_| log_level.into());
     let subscriber = tracing_subscriber::fmt::layer()
         .with_file(true)
         .with_line_number(true)
-        .with_target(false);
+        .with_target(false)
+        .json();
 
     tracing_subscriber::registry()
         .with(subscriber)
         .with(env_filter)
+        // 3
+        .with(opentelemetry)
         .try_init()?;
 
     Ok(())
@@ -85,6 +107,7 @@ async fn bootstrap() -> Result<()> {
     let listener = TcpListener::bind(addr).await?;
     tracing::info!("Listening on {}", addr);
     axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .context("Unexpected error happened in server")
         .inspect_err(|e| {
@@ -92,4 +115,39 @@ async fn bootstrap() -> Result<()> {
                 error.cause_chain = ?e,error.message = %e, "Unexpected error"
             )
         })
+}
+
+async fn shutdown_signal() {
+    fn purge_spans() {
+        global::shutdown_tracer_provider();
+    }
+
+    let ctrl_c = async {
+        tokio::signal::ctrl_c()
+            .await
+            .expect("Failed to install CTRL+C signal handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        tokio::signal::unix::signal(tokio::signal::unix::SignalKind::terminate())
+            .expect("Failed to install SIGTERM signal handler")
+            .recv()
+            .await
+            .expect("Failed to receive SIGTERM signal");
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending();
+
+    tokio::select! {
+        _ = ctrl_c => {
+            tracing::info!("Ctrl-Cを受信しました。");
+            purge_spans()
+        },
+        _ = terminate => {
+            tracing::info!("SIGTERMを受信しました。");
+            purge_spans()
+        }
+    }
 }
